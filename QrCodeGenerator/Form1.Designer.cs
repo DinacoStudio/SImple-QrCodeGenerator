b457c8870683e30db7c01d3e8329070c2f455f74
Dinaco Studio <67733346+DinacoStudio@users.noreply.github.com>
2023-03-20 20:15:42 +0500

    Add files via upload

---
+++ QrCodeGenerator/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace QrCodeGenerator
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.generatebtn = new System.Windows.Forms.Button();
+            this.preview = new System.Windows.Forms.PictureBox();
+            this.param = new System.Windows.Forms.TextBox();
+            this.savebtn = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.preview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // generatebtn
+            // 
+            this.generatebtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.generatebtn.Location = new System.Drawing.Point(12, 317);
+            this.generatebtn.Name = "generatebtn";
+            this.generatebtn.Size = new System.Drawing.Size(109, 31);
+            this.generatebtn.TabIndex = 0;
+            this.generatebtn.TabStop = false;
+            this.generatebtn.Text = "Generate";
+            this.generatebtn.UseVisualStyleBackColor = true;
+            this.generatebtn.Click += new System.EventHandler(this.generatebtn_Click);
+            // 
+            // preview
+            // 
+            this.preview.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.preview.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.preview.Location = new System.Drawing.Point(12, 12);
+            this.preview.Name = "preview";
+            this.preview.Size = new System.Drawing.Size(237, 237);
+            this.preview.TabIndex = 1;
+            this.preview.TabStop = false;
+            // 
+            // param
+            // 
+            this.param.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.param.Location = new System.Drawing.Point(12, 255);
+            this.param.Multiline = true;
+            this.param.Name = "param";
+            this.param.Size = new System.Drawing.Size(237, 56);
+            this.param.TabIndex = 2;
+            this.param.TabStop = false;
+            // 
+            // savebtn
+            // 
+            this.savebtn.Enabled = false;
+            this.savebtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.savebtn.Location = new System.Drawing.Point(127, 317);
+            this.savebtn.Name = "savebtn";
+            this.savebtn.Size = new System.Drawing.Size(122, 31);
+            this.savebtn.TabIndex = 3;
+            this.savebtn.TabStop = false;
+            this.savebtn.Text = "Save";
+            this.savebtn.UseVisualStyleBackColor = true;
+            this.savebtn.Click += new System.EventHandler(this.savebtn_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(261, 360);
+            this.Controls.Add(this.savebtn);
+            this.Controls.Add(this.param);
+            this.Controls.Add(this.preview);
+            this.Controls.Add(this.generatebtn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Qr code generator";
+            ((System.ComponentModel.ISupportInitialize)(this.preview)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button generatebtn;
+        private System.Windows.Forms.PictureBox preview;
+        private System.Windows.Forms.TextBox param;
+        private System.Windows.Forms.Button savebtn;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ QrCodeGenerator/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QrCodeGenerator
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void generatebtn_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(param.Text) || String.IsNullOrWhiteSpace(param.Text)) { MessageBox.Show("Error text cannot be empty"); return; }
+            WebClient web = new WebClient();
+            string url = $"https://chart.googleapis.com/chart?chs=400x400&cht=qr&chl={param.Text}";
+            web.DownloadDataCompleted += (g, k) => {
+                using (var ms = new MemoryStream(k.Result))
+                {
+                    preview.BackgroundImage = Image.FromStream(ms);
+                }
+            };
+            web.DownloadDataAsync(new Uri(url));
+        }
+
+        private void savebtn_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog dlg = new SaveFileDialog())
+            {
+                dlg.Filter = "png|*.png|all files|*.*";
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    preview.BackgroundImage.Save(dlg.FileName);
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            savebtn.Enabled = preview.BackgroundImage!=null;
+        }
+    }
+}
